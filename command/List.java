0cd0204fff45a56e2b9a0ee9be0532bbeccb6904
crtEvent <crtevn@gmail.com>
2023-02-27 17:29:34 +0900

    Add(List): 특정 경로의 파일 목록을 출력하는 runListCommand() 메서드 추가
    특정 경로 문자열을 매개변수로 받아와 해당 경로의 파일 목목을 출력하는 메서드 추가.
    파일 이름과, 용량을 출력한다.

--- command/List.java
+++ command/List.java
@@ -1,4 +1,22 @@
 package command;
 
+import java.io.File;
+import java.text.DecimalFormat;
+
 public class List {
+
+    private static final DecimalFormat df = new DecimalFormat("#,###");
+
+    public void runListCommand(String targetDirectory) {
+        File directory = new File(targetDirectory);
+        File files[] = directory.listFiles();
+
+        for(File file : files) {
+            if(file.isFile()) {
+                System.out.printf("%-20s %13s bytes%n", file.getName(), df.format(file.length()));
+            }
+        }
+
+    }
+
 }
