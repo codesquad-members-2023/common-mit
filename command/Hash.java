1ffc1ecae4210b13e926b7a8a33d8c71c5e2a6b6
crtEvent <crtevn@gmail.com>
2023-02-28 04:50:42 +0900

    Add(Hash): byte[]을 SHA-256 문자열로 변환시켜주는 makeSHA_256() 추가

--- command/Hash.java
+++ command/Hash.java
@@ -1,5 +1,28 @@
 package command;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Hash {
+
+    public String makeSHA_256(byte[] fileContent) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(fileContent);
+
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if(hex.length() == 1) {
+                stringBuffer.append('0');
+            }
+            stringBuffer.append(hex);
+        }
+
+        return stringBuffer.toString();
+    }
+
+
 
 }
