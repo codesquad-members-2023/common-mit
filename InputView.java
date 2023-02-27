5d5ce7d53d95619c3ef6def5be4868504bb4cd22
crtEvent <crtevn@gmail.com>
2023-02-27 17:04:51 +0900

    Add(InputView): 명령어를 입력받는 inputCommand() 메서드 초안 작성
    입력받은 명령어를 3개의 구성 요소로 분해야여 각 명령어를 switch 로 분류하도록 함

--- InputView.java
+++ InputView.java
@@ -1,2 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class InputView {
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private StringTokenizer st;
+
+    public void inputCommand() throws IOException {
+        String[] factors = br.readLine().split(" ");
+
+        switch (factors[1]) {
+            case "list":
+                // 디렉토리의 파일 목록 탐색
+            case "hash":
+                // 디렉토리에 속한 각 파일들 내용의 sha-256 해시코드 출력
+            case "zlib":
+                // 디렉토리에 속한 각 파일들을 zlib으로 압축하여 저장
+            default:
+        }
+    }
 }
