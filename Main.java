6a74586ed9b9aa63712ab378f5b52f0b54a93936
crtEvent <crtevn@gmail.com>
2023-02-28 03:02:47 +0900

    Add(Main): 명령어 에러처리 추가
    명령어가 잘못 되었을 때 -> 다시 입력받기
    입력장치를 인식할 수 없을 때 -> 프로그램 종료

--- Main.java
+++ Main.java
@@ -1,11 +1,18 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         InputView inputView = new InputView();
 
         while(true) {
-            inputView.inputCommand();
+            try {
+                inputView.inputCommand();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (IOException e) {
+                System.out.println("입력장치를 인식할 수 어ꁰ 프로그램을 종료합니다.");
+                System.exit(0);
+            }
         }
 
     }
