f3da3043c705238c6204e7bdbffab1d538f51746
ALbertIM0427 <guang7226961@naver.com>
2023-02-28 07:44:47 +0900

    공통부분 메서드 분리

--- java/src/main/java/org/example/Mit.java
+++ java/src/main/java/org/example/Mit.java
@@ -6,32 +6,34 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.zeroturnaround.zip.ZipUtil;
 
 public class Mit {
 
+
     public List<String> listOfPath(File dir) {
-        File[] files = dir.listFiles();
-        if (files == null) {
-            System.out.println("파일이 없습니다.");
-            return Collections.emptyList();
-        }
-        return Arrays.stream(files)
-            .filter(File::isFile)
-            .map(Mit::makeFileListCommandFormat)
-            .collect(Collectors.toList());
+        return executeCommand(dir, listCommandMapper());
     }
 
     public List<String> hashOfPath(File dir) {
+        return executeCommand(dir, hashCommandMapper());
+    }
+
+    public List<String> executeCommand(File dir,Function<? super File,? extends String> mapper) {
         File[] files = dir.listFiles();
         if (files == null) {
             System.out.println("파일이 없습니다.");
             return Collections.emptyList();
         }
+        return executeCommandExistFiles(files, mapper);
+    }
+
+    private List<String> executeCommandExistFiles(File[] files, Function<? super File,? extends String> mapper) {
         return Arrays.stream(files)
             .filter(File::isFile)
-            .map(Mit::makeHashListCommandFormat)
+            .map(mapper)
             .collect(Collectors.toList());
     }
 
@@ -61,12 +63,11 @@ public class Mit {
 
     }
 
-    private static String makeFileListCommandFormat(File file) {
-        return file.getName() + " " + (file.length()) + "byte";
+    private static Function<? super File,? extends String> listCommandMapper() {
+        return file -> file.getName() + " " + (file.length()) + "byte";
     }
 
-    private static String makeHashListCommandFormat(File file) {
-
-        return file.getName() + " = " + Hashing.sha256().hashString(file.getName(), StandardCharsets.UTF_8);
+    private static Function<? super File,? extends String> hashCommandMapper() {
+        return file -> file.getName() + " = " + Hashing.sha256().hashString(file.getName(), StandardCharsets.UTF_8);
     }
 }
