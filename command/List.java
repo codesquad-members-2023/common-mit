ba984f61e26d0a76c5ba7748695e6dbe62d3160f
crtEvent <crtevn@gmail.com>
2023-02-28 03:03:43 +0900

    Add(List): 파일 목록에 번호 추가
    int index 변수를 이용하여 파일 목록 앞에 번호 추가

--- command/List.java
+++ command/List.java
@@ -9,13 +9,16 @@ public class List {
 
     public void runListCommand(String targetDirectory) {
         File directory = new File(targetDirectory);
-        File files[] = directory.listFiles();
+        File[] files = directory.listFiles();
 
+        int index = 1;
         for(File file : files) {
             if(file.isFile()) {
-                System.out.printf("%-20s %13s bytes%n", file.getName(), df.format(file.length()));
+                System.out.printf("%d. %-20s %13s bytes%n", index++, file.getName(), df.format(file.length()));
             }
         }
+
+        System.out.println();
 
     }
 
