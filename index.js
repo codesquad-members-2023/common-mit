9f772ec0b05dda050114c318ea779e431e255bb8
JH <vvaa277@gmail.com>
2023-02-28 15:16:12 +0900

    1단계 list 명령어 구현
    readline을 통한 입력 프롬프트 구현, showList 함수를 통해 해당경로에 있는 파일을
    순회하며 보여주고 크기를 구한다

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let fs = require("fs");
+
 const readline = require("readline");
 
 const rl = readline.createInterface({
@@ -5,16 +7,41 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const showList = (directoryName) => {
+  fs.readdir(directoryName, (err, filelist) => {
+    if (err) {
+      console.log(err);
+    } else {
+      filelist.forEach((element) => {
+        let stats = fs.statSync(element);
+        let fileSizeInBytes = Math.round(stats.size * 0.001 * 100) / 100;
+        console.log(`${element} ${fileSizeInBytes}KB`);
+      });
+    }
+  });
+};
+
 const inputCommand = () => {
-    console.log("빈 자리는 다음과 같습니다.");
-    rl.on("line", (line) => {
-      const [command, seatNum] = line.split(" ") 
-      switch (command) {
-        case "new":
-          activateUser();
-          break;
-        default:
-           console.log('명령을 다시 입력해주세요 new / stop number / checkuser / checkseat / quit\n')
-      }
-    });
-  };
+  console.log(
+    "명령 형식은 mit {기능} {디렉토리명} 입니다 입력형식에 맞지 않는 경우 종료합니다"
+  );
+  rl.on("line", (line) => {
+    const [mit, command, directoryName] = line.split(" ");
+    switch (command) {
+      case "list":
+        showList(directoryName);
+        break;
+      case "hash":
+        // method();
+        break;
+      case "zlib":
+        // method();
+        break;
+      default:
+        rl.close();
+        console.log("종료합니다");
+    }
+  });
+};
+
+inputCommand();
