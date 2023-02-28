300ec7386f5a0b0f323dbff38ba4fc6702358f97
NANII <breadnanii@gmail.com>
2023-02-28 15:35:10 +0900

    🎨 switch문으로 명령어 기능분리, 중복제거

--- mit.js
+++ mit.js
@@ -12,36 +12,40 @@ rl.setPrompt('> ');
 rl.prompt();
 rl.on('line', line => {
   const [mit, command, directoryPath] = line.split(' ');
-  if(command === 'list') {
+  if(mit === 'mit') {
     const files = fs.readdirSync(directoryPath);
-    for(const file of files) {
-      const filePath = path.resolve(directoryPath, file);
-      const stats = fs.statSync(filePath);
-      const fileSize = (stats.size / 1000).toFixed(2);
-      console.log(`${file} ${fileSize}KB`);
+    switch(command) {
+      case 'list' :
+        for(const file of files) {
+          const filePath = path.resolve(directoryPath, file);
+          const stats = fs.statSync(filePath);
+          const fileSize = (stats.size / 1000).toFixed(2);
+          console.log(`${file} ${fileSize}KB`);
+        }
+        rl.prompt();
+        break;
+      case 'hash' :
+        for(const file of files) {
+          const filePath = path.resolve(directoryPath, file);
+          const data = fs.readFileSync(filePath);
+          const hash = crypto.createHash("sha256").update(data).digest("hex");
+          console.log(`${file} ${hash}`);
+        }
+        rl.prompt();
+        break;
+      case 'zlib' :
+        for(const file of files) {
+          const gzip = zlib.createGzip();
+          const filePath = path.resolve(directoryPath, file);
+          const inputFile = fs.createReadStream(filePath);
+          const outputFile = fs.createWriteStream(filePath+'.z');
+          inputFile.pipe(gzip).pipe(outputFile);
+        }
+        rl.prompt();
+        break;
+      default : rl.close();
     }
+  } else {
+    console.error('mit 명령어를 다시 입력하세요');
   }
-
-  if(command === 'hash') {
-    const files = fs.readdirSync(directoryPath);
-    for(const file of files) {
-      const filePath = path.resolve(directoryPath, file);
-      const data = fs.readFileSync(filePath);
-      const hash = crypto.createHash("sha256").update(data).digest("hex");
-      console.log(`${file} ${hash}`);
-    }
-  }
-
-  if(command === 'zlib') {
-    const files = fs.readdirSync(directoryPath);
-    for(const file of files) {
-      const gzip = zlib.createGzip();
-      const filePath = path.resolve(directoryPath, file);
-      const inputFile = fs.createReadStream(filePath);
-      const outputFile = fs.createWriteStream(filePath+'.z');
-      inputFile.pipe(gzip).pipe(outputFile);
-    }
-  }
-
-  rl.close();
 });
