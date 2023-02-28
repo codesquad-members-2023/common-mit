24a6558d80107b090caef59451c344f201de17ee
saseung <seung0122@naver.com>
2023-02-28 14:57:48 +0900

    feat: readline 활용하여 사용자 입력값 받기
    question으로 한번 입력 받고 그 값을 처리하도록 했다.

---
+++ mitList.js
@@ -0,0 +1,11 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("$ ", (input) => {
+  console.log(input);
+  rl.close();
+});
