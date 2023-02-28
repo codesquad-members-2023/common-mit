310d845a93eb90c9824507e872eccd0ea047af4c
CiRe <dltpwns6@naver.com>
2023-02-28 15:32:14 +0900

    진짜 기능만 구현;;

--- Program.java
+++ Program.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
-import java.io.File;
+import java.io.*;
 import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class Program {
@@ -12,13 +15,17 @@ public class Program {
         this.terminal = new Terminal();
     }
 
-    public void start() {
+    public void start() throws IOException, NoSuchAlgorithmException {
         StringTokenizer st= terminal.getInput();
         setInput(st);
 
         if (option.equalsIgnoreCase("list")) {
             listCommand();
         }
+        else if (option.equalsIgnoreCase("hash")) {
+            hashCommand();
+        }
+
     }
 
 
@@ -34,6 +41,34 @@ public class Program {
 
         for (File file : dir.listFiles()) {
             System.out.println(file.getName() + " " + file.length() + "Byte");
+        }
+    }
+
+    private void hashCommand() throws IOException, NoSuchAlgorithmException {
+        File dir = new File(path);
+        FileFilter fileFilter = new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isFile();
+            }
+        };
+        MessageDigest md = MessageDigest.getInstance(("SHA-1"));
+        for (File file : Objects.requireNonNull(dir.listFiles(fileFilter))) {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            StringBuffer sb = new StringBuffer();
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            md.update(sb.toString().getBytes());
+            byte[] byteData = md.digest();
+            sb.setLength(0);
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i]&0xff) + 0x100, 16).substring(1));
+            }
+
+            System.out.println(file.getName() + " " + sb.toString());
+            sb.setLength(0);
         }
     }
 
