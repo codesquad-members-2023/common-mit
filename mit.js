42a1cdb0f10f10a8a0b031e6551f22ae9f9792fa
jsh3418 <albert5428@gmail.com>
2023-02-28 16:12:37 +0900

    feat: 1단계 list 명령어 구현

---
+++ mit.js
@@ -0,0 +1,48 @@
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("명령어를 입력해주세요.\n", (answer) => {
+  const [system, command, directoryPath] = answer.split(" ");
+
+  if (system !== "mit") {
+    console.log("mit 명령어를 입력해주세요.\n");
+
+    return;
+  }
+
+  switch (command) {
+    case "list":
+      fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+          console.error(err);
+
+          return;
+        }
+
+        files.forEach((file) => {
+          const filePath = path.join(directoryPath, file);
+
+          fs.stat(filePath, (err, stats) => {
+            if (err) {
+              console.error(err);
+
+              return;
+            }
+
+            console.log(`${file} ${stats.size}Byte`);
+          });
+        });
+      });
+
+      break;
+
+    default:
+      console.log("정상적인 명령어가 아닙니다.");
+  }
+});
