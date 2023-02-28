83cad80978995ace6ebedbd228267e988dfc993a
silvertae <massenger7@gmail.com>
2023-02-28 18:47:26 +0900

    Feat: 파일 크기 단위를 바꿔주는 bytesToSize 함수 추가
    기존에 mit list 명령에서 stats.size로 구한 파일의 사이즈 단위는 bytes로만
    출력이 되었다. 이를 1024바이트가 넘어가면 1KB로 바꿔서 출력하도록 도와주는
    bytesToSize 함수를 가져와 추가하였다.

--- list.js
+++ list.js
@@ -1,5 +1,13 @@
 const fs = require('fs/promises');
 const path = require('path');
+// 파일 크기 단위를 바꿔주는 함수
+function bytesToSize(bytes) {
+	const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+	if (bytes === 0) return 'n/a';
+	const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+	if (i === 0) return `${bytes} ${sizes[i]}`;
+	return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
+}
 
 const list = async line => {
 	try {
@@ -8,7 +16,7 @@ const list = async line => {
 		const filelist = await fs.readdir(realPath);
 		for (const file of filelist) {
 			const stats = await fs.stat(`${realPath}/${file}`);
-			console.log(`${file} (${stats.size})`);
+			console.log(`${file} (${bytesToSize(stats.size)})`);
 		}
 	} catch (err) {
 		console.log(err);
