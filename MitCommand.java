d1b85046c520b91fea40c9c833dc3f331b3bd6df
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-02-28 13:14:00 +0900

    feat: hash 메시지 반환타입 변경 및 hash 출력
    기존 File 타입보다 암호화된 해시값을 출력하기 위해서 HashedFile 객체를 새로 만들어 반환하는 것이 좋다고 생각하여 변경하였습니다.
    
    outputHash 함수는 List<HashedFile> 타입 리스트를 받아서 각각의 객체를 출력하는 함수입니다.

--- MitCommand.java
+++ MitCommand.java
@@ -5,13 +5,18 @@ import java.util.function.Consumer;
 
 public interface MitCommand {
     Optional<List<File>> list(String directoryName);
-    Optional<List<File>> hash(String directoryName);
+    Optional<List<HashedFile>> hash(String directoryName);
     Optional<List<File>> zlib(String directoryName);
 
     Consumer<List<File>> outputList = (files->{
         files.stream()
                 .map(file->String.format("%s %.2fKB", file.getName(), (double) file.length() / 1024))
                 .forEach(System.out::println);
+        System.out.println();
+    });
+
+    Consumer<List<HashedFile>> outputHash = (hashedFiles ->{
+        hashedFiles.forEach(System.out::println);
         System.out.println();
     });
 }
