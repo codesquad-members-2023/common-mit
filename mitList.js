fc08b3fd2f148c729fb470882066ac50be27c684
saseung <seung0122@naver.com>
2023-02-28 15:22:00 +0900

    feat: mit 명령어에 따른 함수를 실행하는 mitCommand 함수 추가
    mit 명령어가 총 list, hash, zlib 3개이므로 switch문을 활용했다.

--- mitList.js
+++ mitList.js
@@ -1,5 +1,18 @@
 const readline = require("readline");
 
+function mitCommand(input) {
+  const command = input.split(" ").filter((_, index) => index === 1);
+
+  switch (command) {
+    case "list":
+      break;
+    case "hash":
+      break;
+    case "zlib":
+      break;
+  }
+}
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
