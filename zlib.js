baa8c013a4f08411b660c457d5491a8bdff609f3
aaaz425 <aaaz425@naver.com>
2023-02-28 14:14:49 +0900

    Feat : 각 파일을 압축하는 함수 구현
    디렉토리에서 전체 파일 목록을 탐색하고, 각 파일을 zlib로 압축해서 .z를 붙여서 저장하는 함수 구현

---
+++ zlib.js
@@ -0,0 +1,49 @@
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+import { Transform } from 'stream';
+
+function mitZlib(dirPath) {
+  // 디렉토리 내의 파일 목록 읽어오기
+  const files = fs.readdirSync(dirPath);
+
+  // js 파일만 필터링
+  const jsFiles = files.filter(file => path.extname(file) === '.js');
+
+  // 각 파일을 읽어서 압축하고 .z 파일로 저장
+  jsFiles.forEach((file) => {
+    const filePath = path.join(dirPath, file);
+    const stat = fs.statSync(filePath);
+
+    // 디렉토리인 경우 재귀적으로 호출
+    if (stat.isDirectory()) {
+      mitZlib(filePath);
+    } else {
+      // 파일인 경우 압축하여 .z 파일로 저장
+      const readStream = fs.createReadStream(filePath);
+      const writeStream = fs.createWriteStream(filePath + '.z');
+
+      // 압축 스트림 생성
+      const gzip = zlib.createGzip();
+
+      // 파일 이름과 크기를 기록하는 Transform 스트림 생성
+      const sizeTransform = new Transform({
+        transform(chunk, encoding, callback) {
+          this.totalSize += chunk.length;
+          callback(null, chunk);
+        },
+        flush(callback) {
+          console.log(`${this.fileName}.z ${(this.totalSize / 1024).toFixed(1)}KB`);
+          callback();
+        }
+      });
+      sizeTransform.fileName = file;
+      sizeTransform.totalSize = 0;
+
+      // 파일을 읽고 압축하여 .z 파일에 쓰기
+      readStream.pipe(gzip).pipe(sizeTransform).pipe(writeStream);
+    }
+  });
+}
+
+// mitZlib('./');
