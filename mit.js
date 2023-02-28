37d048bb6ef9ae0a7e03b57d5c0689cd9c8ddd2e
NANII <breadnanii@gmail.com>
2023-02-28 10:02:05 +0900

    ✨ 명령어 입력위한 기본 모듈 설정

---
+++ mit.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', line => {
+  
+});
